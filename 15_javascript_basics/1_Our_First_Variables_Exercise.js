9a71141684d8f19dac0c61bce06104cf6a0983d2
jumpjinda <jumpjinda@gmail.com>
2023-03-15 19:23:18 +0700

    jump

---
+++ 15_javascript_basics/1_Our_First_Variables_Exercise.js
@@ -0,0 +1,9 @@
+// Our First Variables Exercise
+// In index.js, please define TWO variables:
+
+// Define a variable called myLuckyNumber and set it equal to 37.
+
+// Define a variable called octopusLimbs and set it equal to 8.
+
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
